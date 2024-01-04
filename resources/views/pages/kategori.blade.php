e508b365ce7cab731a0220e5bc1b49821d5197e5
oscode04 <hellogaso@gmail.com>
2024-01-04 23:18:33 +0700

    benerin bagian kategori untuk tampilan tab

--- resources/views/pages/kategori.blade.php
+++ resources/views/pages/kategori.blade.php
@@ -5,18 +5,18 @@
 @endsection
 
 @section('content')
-<div class="container-fluid">
+<div class="container-fluid container-category">
 <h1>Kategori</h1>
 <div class="d-lg-flex flex-lg-column align-items-lg-center">
     <div class="row r-1 m-auto">
-    <div class="col-lg-6 col-12 p-0 c-1">
+    <div class="col-lg-6 col-md-6 col-12 p-0 c-1">
         <a href="{{ route('food-tech') }}">
         <div class="frame-kategori-1">
             <img src="../aset/icon/food-tech.jpg" alt="food-tech" />
         </div>
         </a>
     </div>
-    <div class="col-lg-6 col-12 p-0 c-2">
+    <div class="col-lg-6 col-md-6 col-12 p-0 c-2">
         <a href="{{ route('life-style') }}">
         <div class="frame-kategori-2">
             <img src="../aset/icon/life-style.jpg" alt="life-style" />
@@ -25,14 +25,14 @@
     </div>
     </div>
     <div class="row r-2 m-auto">
-    <div class="col-lg-6 col-12 p-0 c-1">
+    <div class="col-lg-6 col-md-6 col-12 p-0 c-1">
         <a href={{ route('healty-food') }}>
         <div class="frame-kategori-2">
             <img src="../aset/icon/healty-food.jpg" alt="healty-food" />
         </div>
         </a>
     </div>
-    <div class="col-lg-6 col-12 p-0 c-2 mb-5">
+    <div class="col-lg-6 col-md-6 col-12 p-0 c-2 mb-5">
         <a href={{ route('edukasi') }}>
         <div class="frame-kategori-1">
             <img src="../aset/icon/education.jpg" alt="food-inno" />
